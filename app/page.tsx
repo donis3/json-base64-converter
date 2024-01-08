5167b540cfec8d8e4f6f008083fa22683f0b70fb
Deniz Ozkan <donis3@gmail.com>
2024-01-08 20:56:19 +0300

    Fix link

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,10 @@ export default function Home() {
 			<section className="p-4 rounded-md bg-white/20 dark:bg-black/20 w-full mt-10 shadow-sm flex flex-col gap-4">
 				<p className="text-base leading-snug tracking-normal text-justify font-light text-primary">
 					A simple tool to convert your JSON objects or strings to
-					base64 and vice versa. You may learn more in the
+					base64 and vice versa. You may learn more in the {" "}
 					<Link href={"/about"} className="underline">
-						{" "}
 						documentation
-					</Link>{" "}
+					</Link>
 					or the{" "}
 					<Link
 						href={process.env.NEXT_PUBLIC_GITHUB_REPO}
